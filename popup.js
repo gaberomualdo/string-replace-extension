f1019c13b026930dd48312b5abd88b1f150c1a0f
xtrp <36894883+xtrp@users.noreply.github.com>
2018-10-21 19:56:01 +0100

    updates

---
+++ popup.js
@@ -0,0 +1,10 @@
+String.prototype.replaceAll = function(str1, str2, ignore) {
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+}
+function replaceWords(w1, w2){
+	document.body.innerHTML = document.body.innerHTML.replaceAll(w1, w2);
+}
+if(document.domain != "mail.google.com"){
+	// eg. replace the word "the" with the word "test"
+	replaceWords("the", "test");
+}
